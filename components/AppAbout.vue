2ce1d1b001712f2554fece023496b670628d03a2
vivanov <brightsdayss@gmail.com>
2024-03-27 18:46:47 +0400

    update about, promo

--- components/AppAbout.vue
+++ components/AppAbout.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="mb-[120px] md:mb-[140px]">
+  <div class="mb-[120px] md:mb-[140px] xl:mb-[180px]">
     <UiCard>
-      <div class="flex flex-col md:flex-row items-center md:items-start gap-6">
-        <img :src=logo alt="logo" class="md:min-w-28 md:px-5 md:py-4"/>
-        <div class="flex flex-col gap-[15px]">
+      <div class="flex flex-col md:flex-row items-center md:items-start xl:items-center gap-6">
+        <div class="md:px-5 xl:pl-20 xl:pr-16 md:py-4 xl:py-12">
+        <img :src=logo alt="logo" class="md:min-w-28 xl:min-w-[135px]"/>
+        </div>
+        <div class="flex flex-col gap-[15px] xl:gap-8">
           <span class="text-2xl md:text-[40px]/[47.4px] md:font-bold text-white text-center md:text-left">About Vue School corporate training</span>
-          <p class="text-sm md:text-[22px/30px] text-white text-center md:text-left">
+          <p class="text-sm xl:text-[22px]/[30px] md:text-[22px/30px] text-white text-center md:text-left">
             School is the #1 training resource for Vue.js and has provided the most 
             comprehensive Vue.js video library in the world to over 140,000 developers. 
             Private individuals and small to enterprise companies have used our services 
@@ -14,9 +16,53 @@
         </div>
       </div>
     </UiCard>
+    <div class="hidden xl:flex justify-between mt-16">
+      <a
+        v-for="item, key of partners"
+        :key="`partner_${item.title}_${key}`"
+        :href="item.url"
+        class="px-[1.4rem] py-2.5"
+      >
+        <img :src="item.logo" :alt="item.title" />
+      </a>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import logo from '../assets/images/logo.svg'
+import google from '../assets/images/partners/google.svg'
+
+const partners = [
+  {
+    title: 'Google',
+    logo: google,
+    url: '/'
+  },
+  {
+    title: 'Google',
+    logo: google,
+    url: '/'
+  },
+  {
+    title: 'Google',
+    logo: google,
+    url: '/'
+  },
+  {
+    title: 'Google',
+    logo: google,
+    url: '/'
+  },
+  {
+    title: 'Google',
+    logo: google,
+    url: '/'
+  },
+  {
+    title: 'Google',
+    logo: google,
+    url: '/'
+  },
+]
 </script>
--- components/AppPromo.vue
+++ components/AppPromo.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="flex flex-col items-center mb-16 md:mb-[6.3rem] pt-8 gap-12">
-    <div class="flex flex-col items-center gap-6 md:gap-10">
-      <h1 class="text-[2.5rem]/[47.4px] md:text-6xl text-white text-center font-bold">Complete 
+  <div class="flex flex-col xl:flex-row items-center xl:mx-[-55px] mb-16 md:mb-[6.3rem] xl:mb-[3.7rem] pt-8 gap-12">
+    <div class="flex flex-col items-center xl:items-start gap-6 md:gap-10">
+      <h1 class="text-[2.5rem]/[47.4px] md:text-6xl text-white text-center xl:text-left font-bold">Complete 
         <span class="text-[#0BD88F]">Vue.js training</span> solutions for companies</h1>
-      <p class="text-base md:text-[22px]/[30px] text-white text-center font-normal">
+      <p class="text-base md:text-[22px]/[30px] text-white text-center xl:text-left font-normal">
         Training solutions designed for companies, agencies and organisations with 
         developers using or who are considering using the Vue.js framework</p>
       <UiButton>Talk to Sales</UiButton>
     </div>
-    <img :src=learners alt="group of learners" class="md:w-[38rem]"/>
+    <img :src=learners alt="group of learners" class="md:min-w-[38rem]"/>
   </div>
 </template>
 
